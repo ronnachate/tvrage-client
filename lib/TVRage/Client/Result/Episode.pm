d872e607939161ddbac1ec156857df18bcb160bf
Ronnachate <yo@abctech-thailand.com>
2014-06-05 17:24:30 +0700

    more fields for episode result.

--- lib/TVRage/Client/Result/Episode.pm
+++ lib/TVRage/Client/Result/Episode.pm
@@ -19,138 +19,115 @@ has 'season' => (
     isa      => 'TVRage::Client::Result::Season',
     required => 1,
 );
-#
-#has 'number' => (
-#    is       => 'ro',
-#    isa      => 'Str',
-#    builder => '_build_number',
-#);
-#
-#has 'title' => (
-#    is => 'ro',
-#    isa => 'Str',
-#    lazy => 1,
-#    builder => '_build_title',
-#);
-#
-#has 'genres' => (
-#    is => 'ro',
-#    isa => 'ArrayRef',
-#    lazy => 1,
-#    builder => '_build_genres',
-#);
-#
-#has 'thumbnails' => (
-#    is      => 'ro',
-#    lazy    => 1,
-#    builder => '_build_thumbnails',
-#);
-#
-#has 'mtags' => (
-#    isa     => 'ArrayRef',
-#    is      => 'ro',
-#    lazy    => 1,
-#    builder => '_build_mtags',
-#);
-#
-#has 'url_safe_title' => (
-#    is => 'ro',
-#    isa => 'Str',
-#    lazy => 1,
-#    builder => '_build_url_safe_title',
-#);
-#
-#sub _build_url_safe_title {
-#    my ($self) = @_;
-#    return $self->urlify($self->title);
-#}
-#
-#sub _build_thumbnails {
-#    my ($self) = @_;
-#    return [ 
-#        TVRage::Client::Result::Image->new( url=> $self->data->{image}->{text} ),
-#    ];
-#}
-#
-#sub _build_mtags {
-#    my ($self) = @_;
-#    my $mtags = [ ];
-#    
-#    push(@$mtags,  { term => "series-id-".$self->id } );
-#    push(@$mtags,  { term => $self->url_safe_title } );
-#    push(@$mtags,  { term => "series-".$self->url_safe_title } );
-#    foreach my $genre ( @{$self->genres} ) {
-#    	push(@$mtags,  { term => $genre } );
-#    } 
-#    return $mtags;
-#}
-#
-#sub _build_number {
-#    my $self = shift;
-#    return $self->client->full_series_data( $self->id )->{Show};
-#}
-#
-#sub _build_season_number {
-#    my $self = shift;
-#    return $self->client->full_series_data( $self->id )->{Show};
-#}
-#
-#sub _build_title {
-#    my ($self) = @_;
-#    return $self->data->{name}->{text};
-#}
-#
-#sub _build_genres {
-#    my ($self) = @_;
-#    my $genres = [];
-#    foreach my $genre ( @{$self->data->{genres}->{genre}} ) {
-#    	push @$genres, $genre->{text};
-#    }
-#    return $genres;
-#}
-#
-#sub type {
-#	return 'series';
-#}
-#
-#=head2 published
-#
-#Returns the published date as a DateTime object
-#
-#=cut
-#
-#sub published {
-#    my ($self) = @_;
-#    return DateTime->now;
-#}
-#
-#
-#=head2 updated
-#
-#Returns the updated date as a DateTime object
-#
-#=cut
-#
-#sub updated {
-#    my ($self) = @_;
-#    return DateTime->now;
-#}
-#
-#sub as_hashref {
-#    my $self = shift;
-#    my %res = (
-#        map({$_ => $self->$_} qw/id title/),
-#        published  => DateTime::Format::Atom->format_datetime($self->published),
-#        updated    => DateTime::Format::Atom->format_datetime($self->updated),
-#        thumbnails => [ {map {$_ => $self->thumbnails->[0]->$_} qw/url width height/} ],
-#        mtags    =>  $self->mtags,
-#        hits     => 0,
-#        duration => 0,
-#        type => $self->type,
-#        url_safe_title => $self->url_safe_title,
-#    );
-#    return \%res;
-#}
+
+has 'number' => (
+    is       => 'ro',
+    isa      => 'Str',
+    builder => '_build_number',
+);
+
+has 'title' => (
+    is => 'ro',
+    isa => 'Str',
+    lazy => 1,
+    builder => '_build_title',
+);
+
+has 'thumbnails' => (
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_thumbnails',
+);
+
+has 'mtags' => (
+    isa     => 'ArrayRef',
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_mtags',
+);
+
+has 'url_safe_title' => (
+    is => 'ro',
+    isa => 'Str',
+    lazy => 1,
+    builder => '_build_url_safe_title',
+);
+
+sub _build_number {
+    my ($self) = @_;
+    return $self->data->{seasonnum}->{text};
+}
+
+sub _build_url_safe_title {
+    my ($self) = @_;
+    return $self->urlify($self->title);
+}
+
+sub _build_thumbnails {
+    my ($self) = @_;
+    return [ 
+        TVRage::Client::Result::Image->new( url=> $self->data->{screencap}->{text} ),
+    ];
+}
+
+sub _build_mtags {
+    my ($self) = @_;
+    my $mtags = [ ];
+    push(@$mtags,  { term => $self->season->serie->url_safe_title.'-s'.$self->season->number} );
+    push(@$mtags,  { term => $self->season->serie->url_safe_title.'-s'.$self->season->number.'-e'.$self->number} );
+    push(@$mtags,  { term => $self->url_safe_title } );
+
+    return $mtags;
+}
+
+sub _build_title {
+    my ($self) = @_;
+    return $self->data->{title}->{text};
+}
+
+
+sub type {
+	return 'episode';
+}
+
+=head2 published
+
+Returns the published date as a DateTime object
+
+=cut
+
+sub published {
+    my ($self) = @_;
+    return DateTime->now;
+}
+
+
+=head2 updated
+
+Returns the updated date as a DateTime object
+
+=cut
+
+sub updated {
+    my ($self) = @_;
+    return DateTime->now;
+}
+
+sub as_hashref {
+    my $self = shift;
+    my %res = (
+        map({$_ => $self->$_} qw/title/),
+        published  => DateTime::Format::Atom->format_datetime($self->published),
+        updated    => DateTime::Format::Atom->format_datetime($self->updated),
+        thumbnails => [ {map {$_ => $self->thumbnails->[0]->$_} qw/url width height/} ],
+        mtags    =>  $self->mtags,
+        hits     => 0,
+        duration => 0,
+        type => $self->type,
+        url_safe_title => $self->url_safe_title,
+    );
+    return \%res;
+}
 
 __PACKAGE__->meta->make_immutable;
 
